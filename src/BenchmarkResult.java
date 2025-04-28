3474d0c819897bcc859325ec1a2275aae146b0ec
Ana <eribacananamaria@gmail.com>
2025-04-28 18:48:42 +0300

    Splitted classes in different files

---
+++ src/BenchmarkResult.java
@@ -0,0 +1,9 @@
+public class BenchmarkResult {
+    public int result;
+    public double time;
+
+    public BenchmarkResult(int result, double time) {
+        this.result = result;
+        this.time = time;
+    }
+}
