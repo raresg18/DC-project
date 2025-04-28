0636ecbbcfa66e6de2d12a284d8022b130f55fd5
Ana <eribacananamaria@gmail.com>
2025-04-28 19:08:50 +0300

    Updated FixedPointIntegerBenchmark and BenchmarkResult with new logic

--- src/BenchmarkResult.java
+++
@@ -1,9 +0,0 @@
-public class BenchmarkResult {
-    public int result;
-    public double time;
-
-    public BenchmarkResult(int result, double time) {
-        this.result = result;
-        this.time = time;
-    }
-}
