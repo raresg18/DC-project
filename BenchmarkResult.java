a137bba5080ba16c75e2984def3c24672390b91a
Ana <eribacananamaria@gmail.com>
2025-04-28 19:21:15 +0300

    Create BenchmarkResult.java

---
+++ BenchmarkResult.java
@@ -0,0 +1,10 @@
+class BenchmarkResult {
+    public int result;
+    public double time;
+
+    public BenchmarkResult(int result, double time) {
+        this.result = result;
+        this.time = time;
+    }
+
+}
